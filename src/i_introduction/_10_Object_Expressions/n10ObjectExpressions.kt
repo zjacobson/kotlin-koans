63e7ef4c6648c94956a6835f0bcecd2cd4461b99
Zak Jacobson <zjacobson@C02M10JCFD57.grubhub.local>
2018-08-03 14:38:33 -0500

    n10

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -3,6 +3,7 @@ package i_introduction._10_Object_Expressions
 import util.TODO
 import util.doc10
 import java.util.*
+import kotlin.Comparator
 
 fun todoTask10(): Nothing = TODO(
     """
@@ -15,9 +16,14 @@ fun todoTask10(): Nothing = TODO(
     """,
     documentation = doc10()
 )
+object comparator : Comparator<Int> {
+    override fun compare(o1: Int?, o2: Int?): Int {
+        return o2!!.compareTo(o1!!)
+    }
 
+}
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, comparator)
     return arrayList
 }
