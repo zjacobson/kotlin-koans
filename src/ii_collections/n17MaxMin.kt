d76373f963dc8e6464968024f9164ab1ca77e407
Zak Jacobson <zjacobson@C02M10JCFD57.grubhub.local>
2018-08-03 15:13:46 -0500

    n17

--- src/ii_collections/n17MaxMin.kt
+++ src/ii_collections/n17MaxMin.kt
@@ -5,12 +5,10 @@ fun example4() {
     val longestString = listOf("a", "b").maxBy { it.length }
 }
 
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? =
     // Return a customer whose order count is the highest among all customers
-    todoCollectionTask()
-}
+    customers.maxBy { it.orders.size }
 
-fun Customer.getMostExpensiveOrderedProduct(): Product? {
+fun Customer.getMostExpensiveOrderedProduct(): Product? =
     // Return the most expensive product which has been ordered
-    todoCollectionTask()
-}
+    orders.flatMap { it.products }.maxBy { it.price }
