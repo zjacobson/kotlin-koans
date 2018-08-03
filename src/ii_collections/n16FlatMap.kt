a994ee522a48a0e03f6ba60f4233bab7ec839900
Zak Jacobson <zjacobson@C02M10JCFD57.grubhub.local>
2018-08-03 15:05:13 -0500

    n16

--- src/ii_collections/n16FlatMap.kt
+++ src/ii_collections/n16FlatMap.kt
@@ -7,12 +7,6 @@ fun example() {
     result == listOf('a', 'b', 'c', '1', '2')
 }
 
-val Customer.orderedProducts: Set<Product> get() {
-    // Return all products this customer has ordered
-    todoCollectionTask()
-}
+val Customer.orderedProducts: Set<Product> get() = orders.flatMap { it.products }.toSet()
 
-val Shop.allOrderedProducts: Set<Product> get() {
-    // Return all products that were ordered by at least one customer
-    todoCollectionTask()
-}
+val Shop.allOrderedProducts: Set<Product> get() = customers.flatMap { it.orderedProducts }.toSet().also {  }
